5bf07ce429fd53d046f53820ab2148f8e0cbfcf8
kim <ngkim623.yahoo.com@gmail.com>
2019-10-29 11:09:40 +0700

    add requirements + bash scripts

--- src/runvisitor.sh
+++ src/runvisitor.sh
@@ -1,2 +1,10 @@
-export PYTHONPATH=$PWD
-python visitor.py --homepages homepages.txt
+#!/bin/bash
+
+export PYTHONPATH=/src
+python3 visitor.py \
+--kafka_host=127.0.0.1 \
+--redis_host=127.0.0.1 \
+--pg_host=10.10.0.12 \
+--pg_user=myuser \
+--pg_password=123456 \
+--pg_relation=data
